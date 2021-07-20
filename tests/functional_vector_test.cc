daccbdb2637f4a4f1766bf78b53387b862914d55
jkaliak <john@kaliakatsos.net>
2021-07-20 08:30:22 +0200

    Default constructor test

--- tests/functional_vector_test.cc
+++ tests/functional_vector_test.cc
@@ -2,11 +2,7 @@
 #include <string>
 #include "functional_vector.h"
 
-// Demonstrate some basic assertions.
-TEST(functional_vector_test, BasicAssertions) {
-    auto t = functional_vector<std::string>({"Mary", "John", "Kate"});
-    // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    EXPECT_EQ(7 * 6, 42);
+TEST(functional_vector_test, default_constructor_test) {
+    auto my_vector = functional_vector<int>();
+    EXPECT_EQ(0, my_vector.size());
 }
