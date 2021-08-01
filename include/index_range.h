cc18ad666eed81e7f3448e8c737264fa14228966
jkaliak <john@kaliakatsos.net>
2021-08-01 19:45:04 +0200

    added documentation in index_range.h

--- include/index_range.h
+++ include/index_range.h
@@ -35,15 +35,50 @@ struct FunctionalVectorExport index_range
     // Create with start and end index (count is calculated)
     static index_range start_end(int start, int end);
     
-    bool operator == (const index_range& rhs) const;
-    bool operator != (const index_range& rhs) const;
-    
+    // The start index of the index range
+    // example:
+    //     [0] [1] [2] [3] [4] [5] [6]
+    //      5   3   9   1   8   3   2
+    //              ^       ^
+    //              |       |
+    //            start    end
+    //
+    //      start = 2
+    //      end = 4
+    //      count = 3
     int start;
+    
+    // The end index of the index range
+    // example:
+    //     [0] [1] [2] [3] [4] [5] [6]
+    //      5   3   9   1   8   3   2
+    //              ^       ^
+    //              |       |
+    //            start    end
+    //
+    //      start = 2
+    //      end = 4
+    //      count = 3
     int end;
+    
+    // The total count of the elements in the index range
+    // example:
+    //     [0] [1] [2] [3] [4] [5] [6]
+    //      5   3   9   1   8   3   2
+    //              ^       ^
+    //              |       |
+    //            start    end
+    //
+    //      start = 2
+    //      end = 4
+    //      count = 3
     int count;
     
-    // Returns if it's safe to use its contents
+    // Returns true if it's safe to use its contents
     bool is_valid;
+    
+    bool operator == (const index_range& rhs) const;
+    bool operator != (const index_range& rhs) const;
     
 private:
     index_range(int start, int count);
